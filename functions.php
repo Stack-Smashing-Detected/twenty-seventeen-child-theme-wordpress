82d15612f34ad3ed40eff0cac23a52c4be57ede5
Victor-J-Wilson <VicJohnathanWilson@outlook.com>
2025-03-08 10:59:38 +0800

    added assets, template-parts header image

--- functions.php
+++ functions.php
@@ -1,19 +1,17 @@
 <?php
 
 //*********Instead of @import Use enqueue ad recommended by WordPress Codex **********
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles()
 {
 
     $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
 
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_register_style(
+    wp_enqueue_style(
         'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array($parent_style),
-        wp_get_theme()->get('Version'),
-        'all'
+        wp_get_theme()->get('Version')
     );
-    wp_enqueue_style('child-style');
 }
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
