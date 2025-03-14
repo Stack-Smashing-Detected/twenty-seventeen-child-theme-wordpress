8a3355f60c4e5f602edb50fe02db0f921c0a0555
Victor-J-Wilson <VicJohnathanWilson@outlook.com>
2025-03-14 22:07:16 +0800

    successfully implemented default post modification on front-page

--- functions.php
+++ functions.php
@@ -2,7 +2,11 @@
 
 //*********Instead of @import Use enqueue ad recommended by WordPress Codex **********
 
-function my_theme_enqueue_styles()
+/**
+ * Enqueues Styles and Scripts
+ * @return void
+ */
+function my_child_theme_enqueue_styles()
 {
     $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
@@ -13,5 +17,25 @@ function my_theme_enqueue_styles()
         wp_get_theme()->get('Version')
     );
 }
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
+
+function modify_homepage_post_action($post) {
+    // get the post on the home page and modify it.
+    $old_content = $post->post_content;
+    if(is_page('home')) {
+        add_filter('the_title', function($title) use ($post) {
+            if(in_the_loop() && is_main_query() && $post->post_title == 'Home'){
+                return 'Welcome!';
+            }
+            return $title;
+        });
+        add_filter('the_content', function($content) use ($post) {
+            if(in_the_loop() && is_main_query() && $post->post_title == 'Home'){
+                return 'This is my developer site, feel free to check out my content or contact me via email or social media.';
+            }
+            return $content;
+        });
+    }
+}
+add_action('the_post', 'modify_homepage_post_action');
 
---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	}
+	?>
+	<p>
+        Copyright © 2017
+    </p>
+</div><!-- .site-info -->
