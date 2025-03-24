29e3d402a258bab6eceee4cde5a9ffddfd0cbcae
VicJo <VicJohnathanWilson@outlook.com>
2025-03-24 23:08:02 +0800

    adapted theme to provide meaningful changes to a laragon installation of wordpress

--- functions.php
+++ functions.php
@@ -1,22 +1,6 @@
 <?php
 
 //*********Instead of @import Use enqueue ad recommended by WordPress Codex **********
-/**
- * Enqueues Styles and Scripts
- * @return void
- */
-function my_child_theme_enqueue_styles()
-{
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
-    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style(
-        'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array($parent_style),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
 
 /**
  * Child widget generator
@@ -41,7 +25,9 @@ add_action('widgets_init', 'my_child_theme_widget_init');
  */
 function modify_homepage_post_action($post): void
 {
-    // get the post on the home page and modify it.
+    // get the post on the home page and modify it, if the wordpress theme
+    // installation comes with a generic index page then filter to the default hello
+    // world post
     if(is_page('home')) {
         add_filter('the_title', function($title) use ($post) {
             if(in_the_loop() && is_main_query() && $post->post_title == 'Home'){
@@ -56,12 +42,44 @@ function modify_homepage_post_action($post): void
             }
             return $content;
         });
+    } else {
+        add_filter('the_title', function($title) use ($post) {
+            if(in_the_loop() && is_main_query() && $post->post_title == 'Hello world!'){
+                return 'Welcome!';
+            }
+            return $title;
+        });
+        add_filter('the_content', function($content) use ($post) {
+            if(in_the_loop() && is_main_query() && $post->post_title == 'Hello world!'){
+                return 'This is a template for a developer website, you can change this content
+                using functions.php with "the_post" action hook in combination with "the_content" filter';
+            }
+            return $content;
+        });
     }
 }
 add_action('the_post', 'modify_homepage_post_action');
 
 /**
- * Modifies the content of posts that are loaded on the other pages (i.e. About, Contact, etc)
+ * Enqueues Styles and Scripts
+ * @return void
+ */
+function my_child_theme_enqueue_styles()
+{
+    $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array($parent_style),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
+
+/**
+ * Modifies the content of posts that are loaded on the other default pages (i.e. About, Contact)
+ * if they exist.
  * @return void
  */
 function modify_post_on_template_load(){
@@ -87,3 +105,5 @@ function modify_post_on_template_load(){
     }
 }
 add_action('template_redirect', 'modify_post_on_template_load');
+
+
